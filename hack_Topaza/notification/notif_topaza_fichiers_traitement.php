2ef794812e84e3b7ee8452c363d47be22d82051e
jbinard <johan.binard@gmail.com>
2013-10-25 10:53:55 +0200

    Modification for compatibility with Agora Project 2.16.3

--- hack_Topaza/notification/notif_topaza_fichiers_traitement.php
+++ hack_Topaza/notification/notif_topaza_fichiers_traitement.php
@@ -19,7 +19,7 @@
 		if(strpos($lien, "index.php")){
 			$lien = rtrim($lien, "index.php");
 		}
-		$lien .= MODULE_DOSSIER."/index.php?id_dossier=".$_POST["id_dossier"];
+		$lien .= "/module_".MODULE_NOM."/index.php?id_dossier=".$_POST["id_dossier"];
 		$contenu_mail .= "<br /> Lien du dossier : ".$lien;
 		envoi_mail($liste_id_destinataires, $objet_mail, $contenu_mail, array("notif"=>true));
 	}
