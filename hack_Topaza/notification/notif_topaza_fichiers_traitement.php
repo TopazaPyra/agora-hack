2965c8c21980950db344423fa2a67763b6b5cc19
jbinard <johan.binard@gmail.com>
2013-10-28 15:02:39 +0100

    Update notif_topaza_fichiers_traitement.php

--- hack_Topaza/notification/notif_topaza_fichiers_traitement.php
+++ hack_Topaza/notification/notif_topaza_fichiers_traitement.php
@@ -1,24 +1,26 @@
 <?php
 //// MODULE FICHIER
 
-	if(isset($_POST["notification"]) && control_upload()==true)
+	if(isset($_POST["notification"]) && control_upload()==true && @$last_id_fichier>0)
 	{
-		// On prends les droits d'acces du dernier fichier OU du fichier auquel on ajoute une version
-		$id_fichier_tmp = (@$_POST["id_fichier_version"]>0)   ?  $fichier_old["id_fichier"]  :  $fichier["id_fichier"];
-		$liste_id_destinataires = users_affectes($objet["fichier"], $id_fichier_tmp);
+		// On prends les droits d'acces du dernier fichier
+		$liste_id_destinataires = users_affectes($objet["fichier"], $last_id_fichier);
 		$nomdossier = db_ligne("SELECT nom FROM gt_fichier_dossier WHERE id_dossier=".$_POST["id_dossier"].";");
 		if($nomdossier['nom']=="") { $nomdossier['nom']="Racine";}
 		$objet_mail = "[".$_SESSION["espace"]["nom"]."] : Fichier - Dossier ".$nomdossier['nom'];
 		$contenu_mail = $trad["FICHIER_mail_nouveau_fichier_cree"]." ".$_SESSION["user"]["nom"]." ".$_SESSION["user"]["prenom"]." :<br /><br />";
 		$contenu_mail .= "Nom(s) du/des fichier(s) ajoute(s) : <br />";
 		foreach($_FILES as $fichier)	{ if($fichier["name"]!="")	$contenu_mail .= $fichier["name"]."<br />"; }
+		$options = array("notif"=>true);
+		if(empty($_POST["notif_joindre_fichiers"]))		$options["fichiers_joints"] = false;
+
 		$lien = $_SESSION["agora"]["adresse_web"];
 		if(strpos($lien, "index.php")){
 			$lien = rtrim($lien, "index.php");
 		}
 		$lien .= "/".MODULE_PATH."/index.php?id_dossier=".$_POST["id_dossier"];
 		$contenu_mail .= "<br /> Lien du dossier : ".$lien;
-		envoi_mail($liste_id_destinataires, $objet_mail, $contenu_mail, array("notif"=>true));
+		envoi_mail($liste_id_destinataires, $objet_mail, $contenu_mail, $options);
 	}
 
 ?>
