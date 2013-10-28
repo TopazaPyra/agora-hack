e970699c2ecbfb3acb37ab56b693c30fc1d8ed17
jbinard <johan.binard@gmail.com>
2013-10-28 09:52:05 +0100

    Allow to attach a file to the notification when the item is modified

--- hack_Topaza/notification/notif_topaza_fichiers.php
+++ hack_Topaza/notification/notif_topaza_fichiers.php
@@ -9,6 +9,13 @@
 		$objet_mail = "[".$_SESSION["espace"]["nom"]."] : Fichier - Dossier ".$nomdossier['nom'];
 		$contenu_mail = "Nom du fichier : ".$_POST["nom"].$_POST["extension"];
 		
+		$_FILES[0]["error"] = 0;
+        	$_FILES[0]["tmp_name"] = PATH_MOD_FICHIER . chemin($objet["fichier_dossier"], $fichier_tmp["id_dossier"], "url") . $fichier_derniere_version["nom_reel"];
+        	$_FILES[0]["name"] = $fichier_tmp["nom"];
+		
+		$options = array("notif"=>true);
+        	if(empty($_POST["notif_joindre_fichiers"]))		$options["fichiers_joints"] = false;
+		
 		$lien = $_SESSION["agora"]["adresse_web"];
 		if(strpos($lien, "index.php")){
 			$lien = rtrim($lien, "index.php");
@@ -17,7 +24,7 @@
 		
 		if($_POST["description"]!="")	{ $contenu_mail .= "<br /><br /> Description du fichier : ".$_POST["description"]."<br />";		}
 		$contenu_mail .= "<br /> Lien du dossier : ".$lien;
-		envoi_mail($liste_id_destinataires, $objet_mail, magicquotes_strip($contenu_mail), array("notif"=>true));
+		envoi_mail($liste_id_destinataires, $objet_mail, magicquotes_strip($contenu_mail), $options);
 	}
 
 ?>
